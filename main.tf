a7577419dca5750f0c57581aa99f9e1bbbb9988f
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-06-29 03:28:16 -0300

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,112 @@
+resource "google_cloud_run_service" "default" {
+
+  depends_on = [
+    google_service_account_iam_member.iam_member,
+    google_service_account_iam_binding.act_as_iam,
+  ]
+
+  name     = var.service_name
+  location = var.cloud_region
+  project  = var.project_id
+
+  template {
+    spec {
+      service_account_name = var.service_name
+
+      containers {
+            image = var.image_url
+
+            ports {
+            container_port = 7700
+            }
+
+            env {
+            name  = "MEILI_HOST"
+            value = var.meili_host
+            }
+
+            env {
+            name  = "MEILI_MASTER_KEY"
+            value = var.meili_master_key
+            }
+
+            env {
+            name  = "MEILI_NO_ANALYTICS"
+            value = var.meili_no_analytics
+            }
+
+            env {
+            name  = "MEILI_ENV"
+            value = var.meili_env
+            }
+
+            env {
+            name  = "TZ"
+            value = var.tz
+            }
+        }
+    }
+
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "5"
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+}
+
+data "google_project" "project" {
+  project_id = var.project_id
+}
+
+resource "google_service_account" "default_service_account" {
+  account_id   = var.service_name
+  display_name = var.service_name
+  project      = data.google_project.project.project_id
+}
+
+resource "google_service_account_iam_binding" "act_as_iam" {
+  service_account_id = google_service_account.default_service_account.name
+  role               = "roles/iam.serviceAccountUser"
+  members = [
+    "serviceAccount:${google_service_account.default_service_account.email}",
+  ]
+}
+
+resource "google_service_account_iam_member" "iam_member" {
+  service_account_id = google_service_account.default_service_account.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.default_service_account.email}"
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+  location = google_cloud_run_service.default.location
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
+
+# gsutil mb -p duckhome-firebase -c STANDARD -l southamerica-east1 gs://duckhome-pps-terraform-state
+terraform {
+  backend "gcs" {
+    bucket = "duckhome-pps-terraform-state"
+    prefix = "terraform/state"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "project_id" {
+  description = "The ID of the project to apply any resources to."
+  type        = string
+}
+
+variable "cloud_region" {
+  description = "The region to deploy to."
+  type        = string
+}
+
+variable "zone" {
+  description = "Cloud zone"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Name of the Cloud Run service."
+  type        = string
+}
+
+variable "image_url" {
+  description = "URL of the Docker image to deploy."
+  type        = string
+}
+
+variable "meili_host" {
+    description = "Server Hostname."
+    type        = string
+}
+
+variable "meili_master_key" {
+    description = "Master key."
+    type        = string
+}
+
+variable "meili_no_analytics" {
+    description = "Disable analytics."
+    type        = string
+}
+
+variable "meili_env" {
+    description = "Environment."
+    type        = string
+}
+
+variable "tz" {
+  description = "Timezone."
+  type        = string
+}
