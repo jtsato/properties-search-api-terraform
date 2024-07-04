e8d6c2e6daca3ef5032fd8c34d310490720fdd86
Jorge Takeshi Sato <jorge.takeshi.sato@gmail.com>
2024-07-04 05:05:43 -0300

    refactor(etl-go): general refactoring

--- etl-go/runner.sh
+++ etl-go/runner.sh
@@ -4,10 +4,12 @@
 if [[ ! -f "./etl-go" ]]; then
   echo "Building etl-go program"
   go build
-  sudo chmod +x etl-go
 else
   echo "Skipping build because etl-go program already exists"
 fi
+
+# Set permissions to execute etl-go program
+sudo chmod +x etl-go
 
 # Run etl-go program
 ./etl-go
